90871eb387a6351ff90d7782a2cd38eeb45df265
roshanroytk-ui <roshanroytk@gmail.com>
2026-05-26 21:03:53 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -250,7 +250,7 @@ for source_data in all_soups:
                     "html.parser"
                 )
 
-                print(detail_response.text[:5000])
+                print(detail_response.text[:1000])
                 break
 
                 detail_scripts = detail_soup.find_all(
